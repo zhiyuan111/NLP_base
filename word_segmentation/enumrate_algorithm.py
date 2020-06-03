a359a3a0c59b4b50d1c69872427bc516cedda115
zhiyuanma <zhiyuanma@hust.edu.cn>
2020-06-03 23:47:50 +0800

    基于枚举算法的分词模型

--- word_segmentation/enumrate_algorithm.py
+++ word_segmentation/enumrate_algorithm.py
@@ -1,43 +1,70 @@
-# TODO: 第一步： 从dic.txt中读取所有中文词。
-#  hint: 思考一下用什么数据结构来存储这个词典会比较好？ 要考虑我们每次查询一个单词的效率。
-dic_words =     # 保存词典库中读取的单词
+import numpy as np
+import xlrd
 
-# 以下是每一个单词出现的概率。为了问题的简化，我们只列出了一小部分单词的概率。 在这里没有出现的的单词但是出现在词典里的，统一把概率设置成为0.00001
-# 比如 p("学院")=p("概率")=...0.00001
-
-word_prob = {"北京":0.03,"的":0.08,"天":0.005,"气":0.005,"天气":0.06,"真":0.04,"好":0.05,"真好":0.04,"啊":0.01,"真好啊":0.02,
-             "今":0.01,"今天":0.07,"课程":0.06,"内容":0.06,"有":0.05,"很":0.03,"很有":0.04,"意思":0.06,"有意思":0.005,"课":0.01,
-             "程":0.005,"经常":0.08,"意见":0.08,"意":0.01,"见":0.005,"有意见":0.02,"分歧":0.04,"分":0.02, "歧":0.005}
-
-print (sum(word_prob.values()))
+dic_path = 'data/综合类中文词库.xlsx'
 
 
-#  分数（10）
-## TODO 请编写word_segment_naive函数来实现对输入字符串的分词
+def create_dic(file_path):
+    workbook = xlrd.open_workbook(file_path)
+    booksheet = workbook.sheet_by_index(0)
+    col_values = booksheet.col_values(0)
+    dic_words = {}
+    max_len_word = 0
+    for word in col_values:
+        dic_words[word] = 0.00001
+        len_word = len(word)
+        if len_word > max_len_word:
+            max_len_word = len_word
+
+    print(len(dic_words))
+    print(max_len_word)
+    return dic_words, max_len_word
+
+
+dic_words, max_len_word = create_dic(dic_path)
+
+word_prob = {"北京": 0.03, "的": 0.08, "天": 0.005, "气": 0.005, "天气": 0.06, "真": 0.04, "好": 0.05, "真好": 0.04, "啊": 0.01,
+             "真好啊": 0.02,
+             "今": 0.01, "今天": 0.07, "课程": 0.06, "内容": 0.06, "有": 0.05, "很": 0.03, "很有": 0.04, "意思": 0.06, "有意思": 0.005,
+             "课": 0.01,
+             "程": 0.005, "经常": 0.08, "意见": 0.08, "意": 0.01, "见": 0.005, "有意见": 0.02, "分歧": 0.04, "分": 0.02, "歧": 0.005}
+
+for key, value in word_prob.items():
+    dic_words[key] = value
+
+
+def word_segmentation(input_str):
+    segments = []
+    if len(input_str) == 0:
+        return segments
+    max_split = min(len(input_str), max_len_word) + 1
+    for idx in range(1, max_split):
+        word = input_str[0:idx]
+        if word in dic_words:
+            segments_substr = word_segmentation(input_str[idx:])
+            if (segments_substr == []) and (len(input_str[idx:]) == 0):
+                segments.append([word])
+            else:
+                for seg in segments_substr:
+                    seg = [word] + seg
+                    segments.append(seg)
+    return segments
+
+
 def word_segment_naive(input_str):
-    """
-    1. 对于输入字符串做分词，并返回所有可行的分词之后的结果。
-    2. 针对于每一个返回结果，计算句子的概率
-    3. 返回概率最高的最作为最后结果
-
-    input_str: 输入字符串   输入格式：“今天天气好”
-    best_segment: 最好的分词结果  输出格式：["今天"，"天气"，"好"]
-    """
-
-    # TODO： 第一步： 计算所有可能的分词结果，要保证每个分完的词存在于词典里，这个结果有可能会非常多。
-    segments = []  # 存储所有分词的结果。如果次字符串不可能被完全切分，则返回空列表(list)
-    # 格式为：segments = [["今天"，“天气”，“好”],["今天"，“天“，”气”，“好”],["今“，”天"，“天气”，“好”],...]
-
-    # TODO: 第二步：循环所有的分词结果，并计算出概率最高的分词结果，并返回
-    best_segment =
-    best_score =
+    segments = word_segmentation(input_str)
+    best_segment = []
+    best_score = np.inf
     for seg in segments:
-    # TODO ...
+        log_prob = -1 * np.sum(np.log([dic_words[word] for word in seg]))
+        if log_prob < best_score:
+            best_segment = seg
+            best_score = log_prob
 
     return best_segment
 
 
 # 测试
-print (word_segment_naive("北京的天气真好啊"))
-print (word_segment_naive("今天的课程内容很有意思")
-print (word_segment_naive("经常有意见分歧"))
+print(word_segment_naive("北京的天气真好啊"))
+print(word_segment_naive("今天的课程内容很有意思"))
+print(word_segment_naive("经常有意见分歧"))
